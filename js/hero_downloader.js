6fed594c5a916b9e5d2a2cf46a9a7838915decec
Kacper "mattrick" Banasik <mattrick@jabster.pl>
2012-09-13 19:56:10 +0200

    hard decision

--- js/hero_downloader.js
+++ js/hero_downloader.js
@@ -3,11 +3,6 @@ function get_icon(skill)
     return (skill) ? 'http://eu.media.blizzard.com/d3/icons/skills/64/' + skill + '.png' : '';
 }
 
-function get_item(item)
-{
-	return (item) ? 'http://eu.media.blizzard.com/d3/icons/items/large/' + item["icon"] + '.png' : '';
-}
-
 function loadImages(sources, callback) {
         var images = {};
         var loadedImages = 0;
@@ -27,49 +22,16 @@ function loadImages(sources, callback) {
         }
       }
 
-CanvasRenderingContext2D.prototype.fillRoundedRect =
-    function (xx,yy, ww,hh, rad, fill, stroke) {
-      if (typeof(rad) == "undefined") rad = 5;
-      this.beginPath();
-      this.moveTo(xx+rad, yy);
-      this.arcTo(xx+ww, yy,    xx+ww, yy+hh, rad);
-      this.arcTo(xx+ww, yy+hh, xx,    yy+hh, rad);
-      this.arcTo(xx,    yy+hh, xx,    yy,    rad);
-      this.arcTo(xx,    yy,    xx+ww, yy,    rad);
-      this.stroke();  // Default to no stroke
-      this.fill();  // Default to fill
-  }; // end of fillRoundedRect method
-
-function addItem(sources, slot)
+function addItem(items, slot, images, context, coordinates)
 {
-    if (sources[slot])
-        $("#equipment #" + slot).append('<img src="' + get_item(sources[slot]) +'" />');
-}
-
-function getQuality(items, images, slot, affix)
-{
-    if (!items[slot])
-        return null;
-
-    switch (items[slot]["displayColor"])
+    if (items[slot])
     {
-        case 'blue':
-            return images["magic" + affix];
-            break;
-
-        case 'yellow':
-            return images["rare" + affix];
-            break;
-
-        case 'orange':
-            return images["legendary" + affix];
-            break;
-
-        case 'green':
-            return images["set" + affix];
-            break;
+        var image = images[items[slot]["displayColor"] + 'Big'];// + (slot == "leftFinger" || slot == "rightFinger" || slot == "neck") ? 'Small' : 'Big';];
+        //context.drawImage(image, coordinates[0], coordinates[1], coordinates[2], coordinates[3]);
+        $("#equipment #" + slot).append('<img src="http://eu.media.blizzard.com/d3/icons/items/large/' + items[slot]["icon"] + '.png" />');
     }
 }
+
 
 var Tool = {
 	data: '',
@@ -90,27 +52,24 @@ var Tool = {
 			var sources = {
 				background: 'img/classes/' + hero_class + '-' + gender + '.png',
 
-				yellow: 'http://eu.battle.net/d3/static/images/item/icon-bgs/yellow.png',
-				gradient: 'http://eu.battle.net/d3/static/images/item/icon-bgs/gradient.png',
-
-				magicBig: 'img/item_backgrounds/magic_big.png',
-				rareBig: 'img/item_backgrounds/rare_big.png',
-				legendaryBig: 'img/item_backgrounds/legendary_big.png',
-				setBig: 'img/item_backgrounds/set_big.png'
+				blueBig: 'img/item_backgrounds/magic_big.png',
+				yellowBig: 'img/item_backgrounds/rare_big.png',
+				orangeBig: 'img/item_backgrounds/legendary_big.png',
+				greenBig: 'img/item_backgrounds/set_big.png'
 			};
 
 			loadImages(sources, function(images) {
 				context.drawImage(images.background, 0, 0, 509, 436, 0, 0, 509, 436);
 
-                if (items["hands"])
-                    context.drawImage(getQuality(items, images, "hands", "Big"), 219, 141, 60, 80);
-
-                if (items["mainHand"])
-                    context.drawImage(getQuality(items, images, "mainHand", "Big"), 219, 294, 60, 124);
+                addItem(items, "head", images, context, [326, 19, 61, 59]);
+                addItem(items, "torso", images, context, [318, 88, 77, 108]);
+                addItem(items, "feet", images, context, [325, 337, 63, 81]);
+                addItem(items, "hands", images, context, [219, 141, 60, 80]);
+                addItem(items, "mainHand", images, context, [219, 294, 60, 124]);
 			});
 
-            addItem(items, "hands");
-            addItem(items, "mainHand");
+
+
 		});
 	},
 
