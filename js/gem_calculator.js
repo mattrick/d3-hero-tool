0f4dd6821eebc341a059bfaabaea254c731f2ca0
Kacper "mattrick" Banasik <mattrick@jabster.pl>
2012-09-14 18:04:55 +0200

    Added selection tooltip

--- js/gem_calculator.js
+++ js/gem_calculator.js
@@ -1,3 +1,6 @@
+var vStarting = 0;
+var vFinal = 0;
+
 $(function () {
     $("form").submit(function() {
         return false;
@@ -6,13 +9,55 @@ $(function () {
 
     });
     $("#starting").click(function() {
-        $.facybox(function() {
-            $.get('gem_selector.html', function(data) {
-                $.facybox(data);
-            });
-        });
+        $.facybox(generateGemSelector("Starting"));
     });
     $("#final").click(function() {
-
+        $.facybox(generateGemSelector("Final"));
     });
 });
+
+function generateGemSelector(which) {
+    var result = '';
+
+    result += '<section id="gem_selector">\n' +
+              '\t<table>\n';
+
+    for (y = 0; y <= 1; y++)
+    {
+        result += '\t\t<tr>\n';
+
+        for (x = 1; x <= 7; x++)
+        {
+            var gem = (y * 7 + x);
+
+            result += '\t\t\t<td>\n' +
+                      '\t\t\t\t<img \n' +
+                      '\t\t\t\t\tonclick=\'gemSelected' + which + '(' + gem +'); $(document).trigger("close.facybox");\'\n' +
+                      ((vStarting >= gem && which == "Final") ? '\t\t\t\t\tstyle="display: none"\n' : '') +
+                      '\t\t\t\t\tsrc="http://eu.media.blizzard.com/d3/icons/items/large/emerald_' + ((gem < 10) ? '0' : '') + gem + '_demonhunter_male.png" />\n' +
+                      '\t\t\t</td>\n';
+        }
+
+        result += '\t\t</tr>\n';
+    }
+
+    result += '\t</table>\n' +
+              '</section>'
+
+    return result;
+}
+
+function gemSelectedStarting(gem) {
+    vStarting = gem;
+    $("#starting").html('<img src="http://eu.media.blizzard.com/d3/icons/items/large/emerald_' + ((gem < 10) ? '0' : '') + gem + '_demonhunter_male.png" />')
+    if (vStarting >= vFinal)
+        gemSelectedFinal(0);
+}
+
+function gemSelectedFinal(gem) {
+    vFinal = gem;
+    if (gem > 0)
+        $("#final").html('<img src="http://eu.media.blizzard.com/d3/icons/items/large/emerald_' + ((gem < 10) ? '0' : '') + gem + '_demonhunter_male.png" />')
+    else
+        $("#final").html('click to choose <strong>final gem</strong>');
+}
